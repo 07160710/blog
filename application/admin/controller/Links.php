5564f55ffc256ef0b985576c4dca5aff343a6bbd
huangkaifeng <1195745286@qq.com>
2018-07-04 22:55:15 +0800

    友情链接功能的部分实现

---
+++ application/admin/controller/Links.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/4
+ * Time: 21:53
+ */
+
+namespace app\admin\controller;
+
+use think\Controller;
+use think\Db;
+use think\Loader;
+use app\admin\model\Links as LinksModel;
+
+class Links extends Controller
+{
+    //列表页面
+    public function lst(){
+        //模型引用
+        $list = LinksModel::paginate(3);
+        $this->assign('list',$list);
+        return $this->fetch();
+    }
+    //添加页面
+    public function add(){
+        if(request()->isPost()){
+            $data = [
+                'title' => input('post.title'),
+                'url' => input('post.url'),
+                'description' => input('post.description'),
+            ];
+
+            //验证数据
+            $validate = Loader::validate('Links');
+            if(!$validate->scene('add')->check($data)){
+                $this->error($validate->getError());
+            }
+
+
+            $res = Db::name('links')->insert($data);
+            if($res){
+                return $this->success('添加链接成功！','lst');
+            }else{
+                return $this->error('添加链接失败');
+            }
+            return;
+        }
+        return $this->fetch();
+    }
+
+    //编辑链接信息
+    public function edit(){
+        //获取id
+        $id = input('id');
+        $results = db('links')->find($id);
+        if(request()->isPost()){
+            $data = [
+                'id' => input('post.id'),
+                'username' => input('post.username'),
+            ];
+
+            //有无传值密码的处理
+            if(input('post.password')){
+                $data['password'] = md5(input('post.password'));
+            }else{
+                $data['password'] = $results['password'];
+            }
+
+            //验证数据
+            $validate = Loader::validate('Links');
+            if(!$validate->scene('edit')->check($data)){
+                $this->error($validate->getError());
+            }
+
+            $res = db('Links')->update($data);
+            if($res){
+                $this->success('修改链接成功','lst');
+            }else{
+                $this->error('修改链接失败');
+            }
+            //加return下面不再运行显示
+            return;
+        }
+
+
+
+        //dump($res);
+        $this->assign('res',$results);
+        return $this->fetch();
+    }
+
+
+    //删除链接模块
+    public function del(){
+        $id = input('id');
+        if($id != 1){
+            $res = db('Links')->delete($id);
+            if($res){
+                $this->success('删除链接成功','lst');
+            }else{
+                $this->error('删除链接失败');
+            }
+        }else{
+            $this->error('初始化链接不能删除');
+        }
+    }
+
+
+}
